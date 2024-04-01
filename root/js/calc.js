9422fe0f7b75ac151a7ccd2a47f92f4105adb4b8
Marcelo Boeira <marcelosteinhaus.ms@gmail.com>
2024-03-31 21:56:41 -0300

    'Calculadora_e_Blog'

---
+++ root/js/calc.js
@@ -0,0 +1,80 @@
+let operador1 = 0;
+let operador2 = 0;
+let operacao = "";
+
+function informarValor(valor) {
+    let display = document.getElementById('display');
+    display.value += valor;
+}
+
+function limpar() {
+    let display = document.getElementById('display');
+    display.value = '';
+}
+function c() {
+    operador1 = 0;
+    operador2 = 0;
+    operacao = "";
+    display.value = "";
+}
+
+function dividir() {
+    operador1 = parseFloat(document.getElementById('display').value);
+    operacao = "/";
+    limpar();
+}
+function multiplicar() {
+    operador1 = parseFloat(document.getElementById('display').value);
+    operacao = "*";
+    limpar();
+}
+function soma() {
+    operador1 = parseFloat(document.getElementById('display').value);
+    operacao = "+";
+    limpar();
+}
+function subtracao() {
+    operador1 = parseFloat(document.getElementById('display').value);
+    operacao = "-";
+    limpar();
+}
+
+
+function resultado() {
+    operador2 = parseFloat(document.getElementById('display').value);
+    if (operacao == "") {
+        alert("OPERAÇÃO INVÁLIDA");
+    } else {
+        let resultado;
+        switch (operacao) {
+            case "/":
+                if (operador2 == 0) {
+                    alert("DIVISÃO POR ZERO");
+                }
+                resultado = operador1 / operador2;
+                break;
+            case "*":
+                if (operador2 == 0) {
+                    alert("MIULTIPLICAÇÃO POR ZERO");
+                }
+                resultado = operador1 * operador2;
+                break;
+            case "+":
+                if (operador2 == 0) {
+                    alert("SOMA POR ZERO");
+                }
+                resultado = operador1 + operador2;
+                break
+            case "-":
+                if (operador2 == 0) {
+                    alert("SUBTRAÇÃO POR ZERO");
+                }
+                resultado = operador1 - operador2;
+                break
+            default:
+                alert("OPERAÇÃO INVÁLIDA");
+        }
+        let display = document.getElementById('display');
+        display.value = resultado;
+    }
+}
